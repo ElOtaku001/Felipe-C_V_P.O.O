b515eaabc732a36c1f6daa023af0d248f11d078b
ElOtaku001 <felipecastro@liceovvh.cl>
2024-09-27 17:41:47 +0200

    ok

---
+++ P.O.O/2.1/script.js
@@ -0,0 +1,13 @@
+function messege(){
+}
+function login(element){
+    if(element.innerText == "Login"){
+        element.innerText == "Logout";
+    }else {
+        element.innerText = "Login";
+    } 
+}
+
+function hide(el){
+    el.remove();
+}
